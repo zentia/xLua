11b0c13eada66a186d937085990d0032b384b51d
zentiali <zentiali@tencent.com>
2024-10-13 20:47:55 +0800

    只使用官方lua 和 luajit

--- build/luac/make_win64.bat
+++ build/luac/make_win64.bat
@@ -1,5 +1,5 @@
 
-set "__VS=Visual Studio 16 2019"
+set "__VS=Visual Studio 17 2022"
 set "__VSWhere=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
 set "__VSDISPLAY="
 set "__VSVER="
@@ -19,7 +19,7 @@ if "%__VSVER%" neq "" (
 
 mkdir build64 & pushd build64
 cmake -DLUAC_COMPATIBLE_FORMAT=ON -G  "%__VS%" -A x64 ..
-IF %ERRORLEVEL% NEQ 0 cmake -DLUAC_COMPATIBLE_FORMAT=ON -G "Visual Studio 16 2019" -A x64 ..
+IF %ERRORLEVEL% NEQ 0 cmake -DLUAC_COMPATIBLE_FORMAT=ON -G "Visual Studio 17 2022" -A x64 ..
 popd
 cmake --build build64 --config Release
 pause
---
+++ cli/unityenv_for_xlua.h
@@ -0,0 +1,8 @@
+﻿
+#ifndef UNITY_2021_1_OR_NEWER
+#define UNITY_2021_1_OR_NEWER
+#endif
+
+#ifndef XLUA_SHARED
+#define XLUA_SHARED
+#endif
